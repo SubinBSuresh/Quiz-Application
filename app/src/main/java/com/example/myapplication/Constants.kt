7c38b28ab27c52aaf398bafa5042f2a2810fe84e
SubinBSuresh <subin.bsuresh27@gmail.com>
2022-09-29 08:12:01 +0530

    Completed MainActivity UI,
    Created a second activity, data class for questions, an object class, UI work is going on in second activity

---
+++ app/src/main/java/com/example/myapplication/Constants.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+object Constants {
+
+    fun getQuestions(): ArrayList<Question>{
+        val questionList = ArrayList<Question>()
+        val question1 = Question(
+            1,"Which animal is this?",
+            R.drawable.ic_launcher_foreground,
+            "Dog","Cat", "Panda", "Rabbit",
+            1
+        )
+
+        val question2 = Question(
+            1,"Which animal is this?",
+            R.drawable.ic_launcher_foreground,
+            "Dog","Cat", "Panda", "Rabbit",
+            3
+        )
+
+        val question3 = Question(
+            1,"Which animal is this?",
+            R.drawable.ic_launcher_foreground,
+            "Dog","Cat", "Panda", "Rabbit",
+            4
+        )
+
+        val question4 = Question(
+            1,"Which animal is this?",
+            R.drawable.ic_launcher_foreground,
+            "Dog","Cat", "Panda", "Rabbit",
+            2
+        )
+
+        val question5 = Question(
+            1,"Which animal is this?",
+            R.drawable.ic_launcher_foreground,
+            "Dog","Cat", "Panda", "Rabbit",
+            2
+        )
+
+        questionList.add(question1)
+        questionList.add(question2)
+        questionList.add(question3)
+        questionList.add(question4)
+        questionList.add(question5)
+        return questionList
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_start : Button = findViewById(R.id.btn_start)
+        val tv_name : TextView = findViewById(R.id.tv_name)
+        val user_name = tv_name.text
+
+        btn_start.setOnClickListener{
+            if (user_name.isNotEmpty()){
+                val intent : Intent = Intent(this,QuestionsActivity::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "Name cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
